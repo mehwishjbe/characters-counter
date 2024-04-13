c76530cdc9a0de7645d43ca21236f62931710348
mehwishjbe <mehwish.jbe@gmail.com>
2024-04-13 23:41:53 +0200

    accessing total counter element js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const textArea = document.getElementById("textarea");
+const totalCounter = document.getElementById("total-counter");
 
 textArea.addEventListener("keyup", () => {
   console.log("key is pressed");
