bb6500421967b0ad30c8082c2a76d7ec174d4639
mehwishjbe <mehwish.jbe@gmail.com>
2024-04-13 23:45:38 +0200

    accessing remaining counter element js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const textArea = document.getElementById("textarea");
 const totalCounter = document.getElementById("total-counter");
+const remainingCounter = document.getElementById("remaining-counter");
 
 textArea.addEventListener("keyup", () => {
   console.log("key is pressed");
